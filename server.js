0eec09d67acb4ac21dd8ddad53fd00770d2648d4
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-03-29 11:22:08 +0800

    - Implemented Password encryption

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import connectDB from './models/db.js';
 import express from 'express';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 var app = express();
 connectDB();
 
 app.use(express.json());
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', function (request, response) {
     response.send('Hello Node JS');
