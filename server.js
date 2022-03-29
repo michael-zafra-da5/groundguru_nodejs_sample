000170ebdb5ef88e93f00a6527447d3aaf107a74
zafyraminx <80800798+michael-zafra-da5@users.noreply.github.com>
2022-03-29 14:57:28 +0800

    - Added CORS

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import connectDB from './models/db.js';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import cors from 'cors';
 
 var app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
